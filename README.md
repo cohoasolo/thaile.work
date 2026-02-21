52068949f201ba5db6e4f0e2e38c9cd828cea421
cohoasolo <cohoasolo@gmail.com>
2026-02-21 17:43:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# thaile.work
---
+++ js/main.js
@@ -0,0 +1,24 @@
+// my current time
+
+let time = document.getElementById('current-time');
+let date = document.getElementById('current-date');
+
+// specify the home timezone (IANA identifier)
+const homeTimeZone = 'Asia/Ho_Chi_Minh'; // change as needed
+
+setInterval(() =>{
+    let d = new Date(); // Current date and time in UTC internally
+    // format using the desired timezone regardless of visitor's system
+    date.innerHTML = d.toLocaleDateString(undefined, { timeZone: homeTimeZone });
+    time.innerHTML = d.toLocaleTimeString(undefined, { timeZone: homeTimeZone });
+},1000)
+
+// hamburger nav
+$('.nav-toggle').on('click', function() {
+
+    $('.nav_container').toggleClass('no-scrollbar')
+    $('.nav').toggleClass('open')
+
+    return false
+
+})

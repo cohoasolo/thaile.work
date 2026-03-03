6f6b8d1905be06f1350d0cb0c89aea67b0dd3ce6
cohoasolo <cohoasolo@gmail.com>
2026-03-03 10:41:27 +0700

    Refactor navigation and project links; update cover image positioning logic; add new project pages and selected works section

--- js/cover-image.js
+++ js/cover-image.js
@@ -1,13 +1,22 @@
 // Set --pageMargin variable from .page_container {margin-left} only if 1025px < screen width <= 1300px or screen width >= 2080px , in order to position projects' .cover-image
 let pageMargin = 0;
-document.addEventListener('DOMContentLoaded', function() {
-    setInterval(() =>{
-        if ((window.innerWidth > 1025 && window.innerWidth <= 1300 ) || (window.innerWidth >= 2080)) {
-            const pageContainer = document.querySelector('.page_container');
-            if (pageContainer) {
-                const marginLeft = getComputedStyle(pageContainer).marginLeft;
-                pageContainer.style.setProperty('--pageMargin', marginLeft);
-            }
+
+const coverImgPosition = function() {
+    if ((window.innerWidth > 1025 && window.innerWidth <= 1300 ) || (window.innerWidth >= 2080)) {
+        const pageContainer = document.querySelector('.page_container');
+        if (pageContainer) {
+            const marginLeft = getComputedStyle(pageContainer).marginLeft;
+            pageContainer.style.setProperty('--pageMargin', marginLeft);
         }
-    },200)
-});
+    }
+}
+
+// position cover image on load
+document.addEventListener('DOMContentLoaded', function() {
+    coverImgPosition()
+})
+
+// position cover image on browser resize
+window.addEventListener("resize", function() {
+    coverImgPosition()
+})
--- js/nav.js
+++ js/nav.js
@@ -13,21 +13,58 @@ const toggleTag = document.querySelector("a.nav-toggle")
 const navContainerTag = document.querySelector(".nav_container")
 const navTag = document.querySelector("nav")
 
+// on content pages, when i scroll down, at a certain point
+// add a class to make the nav toggle has white background
+const bgToggle = function() {
+    const pixels = window.pageYOffset
+
+    if (pixels > 60) {
+        toggleTag.classList.add("background")
+    } else {
+        toggleTag.classList.remove("background")
+    }
+}
+
+bgToggle()
+
+// check if nav is open when scroll
+document.addEventListener("scroll", function() {
+    if ((!navContainerTag.classList.contains("appear")) || (!navTag.classList.contains("open"))) {
+        bgToggle()
+    }
+})
+
 // when i click the toggle tag, toggle a class of appear on the nav container tag, and open on the nav tag
 toggleTag.addEventListener("click", function (event) {
     // prevent the default action of the anchor tag (in jQuery this is default for a tag with href="#" )
     event.preventDefault()
-    
-    // toggle class
-    navContainerTag.classList.toggle("appear")
-    navTag.classList.toggle("open")
 
-    // check if nav is open, change the toggle icon to close and remove background color
-    if ((navContainerTag.classList.contains("appear")) && (navTag.classList.contains("open"))) {
-            toggleTag.innerHTML = '<img src="/assets/close.svg">'
-            toggleTag.classList.add("no-background")
+    // to open, add class appear and open to the nav container and nav tag respectively
+    // then change the toggle icon to close and remove background color
+    // add delay for smooth animation of nav and nav container tag
+    if ((!navContainerTag.classList.contains("appear")) || (!navTag.classList.contains("open"))) {
+        
+        navContainerTag.classList.add("appear")
+        
+        setTimeout (function() {
+            navTag.classList.add("open")
+        })
+        
+        toggleTag.innerHTML = '<img src="/assets/close.svg">'
+        toggleTag.classList.remove("background")       
+
     } else {
+
+        navTag.classList.remove("open")
+
+        setTimeout (function() {
+            navContainerTag.classList.remove("appear")
+        }, 400)
+
         toggleTag.innerHTML = '<img src="/assets/hamburger.svg">'
-        toggleTag.classList.remove("no-background")
+        toggleTag.classList.add("background") 
+
     }
 })
+
+
